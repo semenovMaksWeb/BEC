2ff84c3757e3eeaedd794cbd4a400ebcf3a2fd63
Semenov <semenov@mail.ru>
2023-01-29 14:19:22 +0400

    BEC-35 обработка excel

--- src/main/java/com/example/bec/controller/CommandController.java
+++ src/main/java/com/example/bec/controller/CommandController.java
@@ -1,7 +1,9 @@
 package com.example.bec.controller;
 
+
 import com.example.bec.service.CommandService;
 import io.swagger.annotations.ApiParam;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
@@ -29,8 +31,15 @@ public class CommandController {
             @RequestBody Map<String, Object> params,
             @RequestHeader(name="Authorization", required=false) String token
 
-    ) throws IOException, SQLException, MessagingException {
+    ) throws IOException, SQLException, MessagingException, InvalidFormatException {
         params.put("token", token);
         return this.commandService.run(name, params);
+    }
+    @RequestMapping(
+            value = "test",
+            method = RequestMethod.POST
+    )
+    public Object test() throws Exception {
+        return null;
     }
 }
